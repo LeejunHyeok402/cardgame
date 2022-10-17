935774823703f8711179f9a607ad8803d85bacad
LeejunHyeok402 <wnsgur970718@gmail.com>
2022-10-17 21:04:47 +0900

    1017 cardgame

--- myajax.py
+++ myajax.py
@@ -14,8 +14,11 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 @app.get("/zzang", response_class=HTMLResponse)
-async def selects(request: Request):
+async def zzang(request: Request):
     return templates.TemplateResponse("zzang.html", {"request": request})
 
 
+@app.get("/zzang2", response_class=HTMLResponse)
+async def zzang2(request: Request):
+    return templates.TemplateResponse("zzang2.html", {"request": request})
 #uvicorn myajax:app --reload
